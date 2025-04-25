52a93b4e13d74e57a58f9233e0cec9589a89c0d4
Mohamedzonol <mo.zonkol@gmail.com>
2025-04-25 19:12:38 +0300

    last change

--- utils/api/AlertsServices.ts
+++ utils/api/AlertsServices.ts
@@ -1,16 +1,13 @@
 import { EmergencyAlert } from "../types";
-
 export const fetchAllAlerts = async () => {
     try {
         const response = await fetch(
-            // "https://crop-pilot-api.azurewebsites.net/api/Dashbored/GetAlerts");
-            "http://localhost:5070/api/Dashbored/GetAlerts");
+            "https://crop-pilot-api.azurewebsites.net/api/Dashbored/GetAlerts");
+        // "http://localhost:5070/api/Dashbored/GetAlerts");
         if (!response.ok) {
             throw new Error("Failed to fetch Alert data");
         }
-
         const data = await response.json();
-        console.log(data.data);
         return data.data.map((apiAlert: any) => ({
             id: apiAlert.alertId,
             type: apiAlert.alertType,
@@ -24,7 +21,6 @@ export const fetchAllAlerts = async () => {
         }));
 
     } catch (error) {
-        console.error("Error fetching Alert data:", error);
         throw error;
     }
 };
@@ -33,22 +29,24 @@ export const createAlert = async (alertData: EmergencyAlert) => {
     try {
         const location = alertData.locationDescription || "Unknown Location";
 
-        // Make the POST request to the API
-        const response = await fetch("http://localhost:5070/api/Dashbored/CreateAlert", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-                emergencyType: alertData.type,
-                message: alertData.message,
-                severity: alertData.severity,
-                latitude: alertData.latitude,
-                longitude: alertData.longitude,
-                locationDescription: location,
-                createdAt: new Date().toISOString(),
-            }),
-        });
+        const response = await fetch(
+            // "http://localhost:5070/api/Dashbored/CreateAlert"
+            "https://crop-pilot-api.azurewebsites.net/api/Dashbored/CreateAlert"
+            , {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    emergencyType: alertData.type,
+                    message: alertData.message,
+                    severity: alertData.severity,
+                    latitude: alertData.latitude,
+                    longitude: alertData.longitude,
+                    locationDescription: location,
+                    createdAt: new Date().toISOString(),
+                }),
+            });
 
         if (!response.ok) {
             throw new Error(`Failed to create Alert: ${response.statusText}`);
